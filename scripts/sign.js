ebcfee11c9b80f3bdc112691bbfee2bfcbe88291
=?UTF-8?q?=E2=80=8E?= <107724647+username@users.noreply.github.com>
2019-02-07 00:49:59 +0500

    Added signing script using ethers.js

---
+++ scripts/sign.js
@@ -0,0 +1,28 @@
+//Users ethers.js
+const ethers = require("ethers");
+const Wallet = ethers.Wallet;
+const utils = ethers.utils;
+
+let mnemonic =
+  "blur depend exhibit cliff film govern toward type embrace fine latin hotel";
+
+let wallet = ethers.Wallet.fromMnemonic(mnemonic);
+console.log("Wallet address:", wallet.address);
+
+console.log("Private key:", wallet.privateKey);
+
+let message = "Dummy message to sign";
+console.log("Message to sign:", message);
+
+let nonce = 0;
+
+let msgHash = utils.solidityKeccak256(["string", "uint256"], [message, nonce]);
+console.log("Message hash:", msgHash);
+
+let hashToSign = utils.arrayify(msgHash);
+
+let signature = wallet.signMessage(hashToSign);
+console.log("Signature", signature);
+
+let signer = Wallet.verifyMessage(hashToSign, signature);
+console.log("Signing address:", signer);
